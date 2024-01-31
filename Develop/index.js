54d400edf4482e39ea4dbd621d6d04da3199bb34
VictoriaSweet <sweet.victoria1218@gmail.com>
2024-01-31 12:52:00 -0600

    fixed answer input and generate index.html function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 const questions = [];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+
 function init() {}
 
-// Function call to initialize app
 init();
+const generateHTML = ({ name, description, installation, usage,testing, license }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="p-5 mb-4 header bg-light">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${name}</h1>
+      <p class="lead">My project is ${description}.</p>
+      <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">You are able to start installation for this application by ${installation}</li>
+        <p class="lead">the use for this application is ${usage}.</p>
+        <p class="lead">The testing applications I have used were ${testing}.</p>
+        <p class="lead">The license I have chosen was ${license}.</p>
+      </ul>
+    </div>
+  </header>
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your project name?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'what is the description of your title?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'instructions for installing the application',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'any usage information on how to start the application?',
+    },
+    {
+      type: 'input',
+      name: 'Contributions',
+      message: 'Enter the information of who contributed to the repo',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'Enter your applications used for testing.',
+    },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Enter your chosen license',
+      },
+
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
--- README.md
+++ README.md
@@ -1,22 +1,20 @@
-# dreamy-trash
+# Shreekin-and-Geekin
 
 ## Description
-This is a webpage where you can document your daily activities from the hours of 9am-5pmm, it will show past present and future as well as a current date and time box. You can save the data, hit refresh and they will still be stored on th screen. 
+This is a back end application where you are able to insert the requirements for your README.md, then the README.md is auto generated with the inputs you have provided. 
 
 ## Installation
-$npm install
+`$node index.js`
 
 # Usage
 This application is a useful tool used to take notes offline as well as reference the material once the browser is closed and without internet connection. You are able to click the  install button  and download an icon of the application on your home page. 
 
 ## Credits
-starter code provided from the repo of https://github.com/coding-boot-camp/cautious-meme
+starter code provided from the repo of https://github.com/coding-boot-camp/potential-enigma
 
 ## License
 MIT
 
-## Screenshot of deployed application
-![screenshot of the deployed application](image.png)
 
 ### Questions?
  <a href="mailto:sweet.victoria1218@gmail.com">Email Me</a>
